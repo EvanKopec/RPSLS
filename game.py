ee350a337dbb0b89edd9ad38a26d65ff27d16303
Evan Kopec <ekopec0705@gmail.com>
2022-05-16 14:36:29 -0500

    Files set up and outlines completed

---
+++ game.py
@@ -0,0 +1,6 @@
+class Game:
+    def __init__(self):
+        pass
+
+    def display_welcome(self):
+        print('Welcome')
---
+++ player.py
@@ -0,0 +1,6 @@
+class player:
+    def __init__(self):
+        self.gestures = ['Rock, Paper, Scissors, Lizard, Spock']
+        self.wins = int
+
+    

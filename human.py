a9737fede4eb5e8a0d75f8fbe732193ce0321b1b
Evan Kopec <ekopec0705@gmail.com>
2022-05-17 15:23:08 -0500

    add self to selected_gesture

--- human.py
+++ human.py
@@ -10,24 +10,24 @@ class Human(Player):
         while gesture is False:
             selected_gesture = int(input('0 for rock, 1 for paper, 2 for scissors, 3 for lizard, 4 for spock'))
             if selected_gesture == 0:
-                selected_gesture = self.gestures[0]
+                self.selected_gesture = self.gestures[0]
                 gesture is True
-                print(selected_gesture)
+                print(self.selected_gesture)
             elif selected_gesture == 1:
-                selected_gesture = self.gestures[1]
+                self.selected_gesture = self.gestures[1]
                 gesture = True
             elif selected_gesture == 2:
-                selected_gesture = self.gestures[2]
+                self.selected_gesture = self.gestures[2]
                 gesture = True
-                print(selected_gesture)
+                print(self.selected_gesture)
             elif selected_gesture == 3:
-                selected_gesture = self.gestures[3]
+                self.selected_gesture = self.gestures[3]
                 gesture = True
             elif selected_gesture == 4:
-                selected_gesture = self.gestures[4]
+                self.selected_gesture = self.gestures[4]
                 gesture = True
-        else:
-            print('Thats not a option, try again!')
+            else:
+                print('Thats not a option, try again!')
       
     
         
